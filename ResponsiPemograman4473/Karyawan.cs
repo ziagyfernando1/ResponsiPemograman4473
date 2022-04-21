8b181f1f36b777fd1e9ef7df3e18167f2b65260e
ziagyfernando1 <ziagyfernando1@students.amikom.ac.id>
2022-04-21 09:49:19 +0700

    Add project files.

---
+++ ResponsiPemograman4473/Karyawan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karyawan
+{
+    public class Program
+    {
+        static void Main(String[]args)
+        {
+            Karyawan karyawan = new Karyawan();
+
+            karyawan.NikPaijo = 190302123;
+            karyawan.NikJono = 190302124;
+            karyawan.Nama1 = "Paijo";
+            karyawan.Nama2 = "Jono";
+            karyawan.GajiBulanan1 = 3000000;
+            karyawan.GajiBulanan2 = 2000000;
+
+            karyawan.KaryawanInfo();
+            karyawan.KenaikanGaji();
+        }
+    }
+}
---
+++ ResponsiPemograman4473/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karyawan
+{
+    public class Karyawan
+    {
+        //properties
+        public int NikPaijo { get; set; }
+        public int NikJono { get; set; }
+        public string Nama1 { get; set; }
+        public string Nama2 { get; set; }
+        public float GajiBulanan1 { get; set; }
+        public float GajiBulanan2 { get; set; }
+
+        //method
+        public void KaryawanInfo()
+        {
+            Console.WriteLine("Nik: {0}", NikPaijo);
+            Console.WriteLine("Nik: {0}", NikJono);
+            Console.WriteLine("Nama: {0}", Nama1);
+            Console.WriteLine("Nama: {0}", Nama2);
+            Console.WriteLine("GajiBulanan: {0}\n", GajiBulanan1);
+            Console.WriteLine("GajiBulanan: {0}\n", GajiBulanan2);
+        }
+        public void KenaikanGaji()
+        {
+            Console.WriteLine("Nik: {0}", NikPaijo);
+            Console.WriteLine("Nik: {0}", NikJono);
+            Console.WriteLine("Nama: {0}", Nama1);
+            Console.WriteLine("Nama: {0}", Nama2);
+            Console.WriteLine("GajiBulanan: {0}\n", GajiBulanan1+30.000);
+            Console.WriteLine("GajiBulanan: {0}\n", GajiBulanan2+20.000);
+        }
+       
+
+    }
+}
